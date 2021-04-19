5a2ba5a0735cc01bf359e67c4c8713b8989cb42c
Manuel Jiménez Romero <manuikejim95@outlook.com>
2021-04-19 23:55:13 +0200

    Añadir usuarios

---
+++ models/Users.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property int|null $posts
+ * @property int|null $comments
+ * @property int $role
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password', 'role'], 'required'],
+            [['posts', 'comments', 'role'], 'default', 'value' => null],
+            [['posts', 'comments', 'role'], 'integer'],
+            [['username'], 'string', 'max' => 25],
+            [['password'], 'string', 'max' => 255],
+            [['username'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'posts' => 'Posts',
+            'comments' => 'Comments',
+            'role' => 'Role',
+        ];
+    }
+}
